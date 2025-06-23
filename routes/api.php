6d36ef607765fb91edbad42a3d2b38f3d46608a0
Isuru Bandara <isurubandara318@gmail.com>
2025-06-23 13:17:59 +0530

    Auth API

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('/logout', function (Request $request) {
+    $request->user()->currentAccessToken()->delete();
+    return response()->noContent();
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/test', [AuthController::class, 'test']);
+
