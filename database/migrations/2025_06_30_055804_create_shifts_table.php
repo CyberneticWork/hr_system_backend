4e3c289902020409ee21074fa789bc139b329e61
Isuru Bandara <isurubandara318@gmail.com>
2025-06-30 11:31:05 +0530

    Add shifts model, migration, and seed data
    Introduced a new shifts Eloquent model and created a migration for the shifts table with relevant fields. Updated the DatabaseSeeder to populate the shifts table with initial shift records.

---
+++ database/migrations/2025_06_30_055804_create_shifts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shifts', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('shift_code')->unique();
+            $table->string('shift_description');
+
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->time('morning_ot_start')->nullable();
+            $table->time('special_ot_start')->nullable();
+            $table->time('late_deduction')->nullable();
+
+            $table->boolean('midnight_roster')->default(false);
+            $table->decimal('nopay_hour_halfday', 4, 2)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shifts');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\departments;
 use App\Models\employment_type;
+use App\Models\shifts;
 use App\Models\sub_departments;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -74,6 +75,41 @@ class DatabaseSeeder extends Seeder
                     ]);
                 }
             }
+        }
+
+        $shifts = [
+            ['001', 'No OT - WD', '08:00:00', '17:00:00', '08:00:00', '17:00:00', '00:00:00', false, 4.5],
+            ['002', 'Snr Mgt - FM', '08:15:00', '17:15:00', '08:15:00', '17:15:00', '00:00:00', false, 4.5],
+            ['003', 'Snr Mgt - Purchasing', '07:45:00', '16:45:00', '07:45:00', '16:45:00', '00:00:00', false, 4.5],
+            ['004', 'Office Executive - WD', '08:00:00', '17:00:00', '08:00:00', '17:00:00', '08:00:00', false, 5.0],
+            ['005', 'Office Executive - WE', '08:00:00', '13:00:00', '08:00:00', '13:00:00', '08:00:00', false, 5.0],
+            ['006', 'Production Mgr - WD', '08:00:00', '17:00:00', '07:30:00', '17:15:00', '08:00:00', false, 4.5],
+            ['007', 'Production Mgr - WE', '08:00:00', '13:00:00', '07:30:00', '17:15:00', '08:00:00', false, 5.0],
+            ['008', 'QC - WE', '08:00:00', '13:00:00', '08:00:00', '17:00:00', '08:00:00', false, 5.0],
+            ['009', 'No OT with Late - WD', '08:00:00', '17:00:00', '08:00:00', '17:00:00', '08:00:00', false, 4.5],
+            ['010', 'Production - WD', '08:00:00', '17:00:00', '07:30:00', '17:00:00', '08:00:00', false, 5.0],
+            ['011', 'Production - WE', '08:00:00', '13:00:00', '07:30:00', '17:00:00', '08:00:00', false, 5.0],
+            ['012', 'Production - Trainee', '08:00:00', '17:00:00', '08:00:00', '17:00:00', '08:00:00', false, 4.5],
+            ['013', 'No OT with Late - WE', '12:00:00', '17:00:00', '12:00:00', '17:00:00', '12:00:00', false, 5.0],
+            ['014', 'OT with Late - WE', '12:00:00', '17:00:00', '11:30:00', '17:00:00', '12:00:00', false, 5.0],
+            ['015', 'Security - WD Morning', '07:00:00', '15:00:00', '07:00:00', '19:00:00', '07:00:00', false, 4.0],
+            ['016', 'Security - WD Night', '19:00:00', '03:00:00', '07:00:00', '07:00:00', '07:00:00', true, 4.0],
+            ['017', 'Security - WE Morning', '07:00:00', '15:00:00', '07:00:00', '19:00:00', '07:00:00', false, 5.0],
+            ['018', 'Security - WE Night', '19:00:00', '00:00:00', '07:00:00', '07:00:00', '07:00:00', true, 5.0],
+        ];
+
+        foreach ($shifts as $shift) {
+            shifts::create([
+                'shift_code' => $shift[0],
+                'shift_description' => $shift[1],
+                'start_time' => $shift[2],
+                'end_time' => $shift[3],
+                'morning_ot_start' => $shift[4],
+                'special_ot_start' => $shift[5],
+                'late_deduction' => $shift[6],
+                'midnight_roster' => $shift[7],
+                'nopay_hour_halfday' => $shift[8],
+            ]);
         }
 
     }
