563ae9976fac18d8ae96a9ff5a603e5575a513e5
Isuru Bandara <isurubandara318@gmail.com>
2025-07-02 17:01:53 +0530

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
-        return response()->json($users);
+        return response()->json($users,200);
     }
 
     /**
@@ -59,7 +59,7 @@ class UserController extends Controller
         if (!$user) {
             return response()->json(['message' => 'User not found'], 404);
         }
-        return response()->json($user);
+        return response()->json($user, 200);
     }
 
     /**
