26bd7adb2e5c465cb54ead5f263846a3874524f8
Isuru Bandara <isurubandara318@gmail.com>
2025-07-15 09:53:40 +0530

    Update company.php

--- app/Models/company.php
+++ app/Models/company.php
@@ -13,4 +13,23 @@ class company extends Model
     {
         return $this->hasMany(departments::class);
     }
+
+    // Relationship: company → organization_assignments
+    public function organizationAssignments()
+    {
+        return $this->hasMany(\App\Models\organization_assignment::class, 'company_id');
+    }
+
+    // Employees through organization_assignments
+    public function employees()
+    {
+        return $this->hasManyThrough(
+            employee::class,
+            organization_assignment::class,
+            'company_id', // Foreign key on organization_assignments
+            'organization_assignment_id', // Foreign key on employees
+            'id', // Local key on companies
+            'id' // Local key on organization_assignments
+        );
+    }
 }
