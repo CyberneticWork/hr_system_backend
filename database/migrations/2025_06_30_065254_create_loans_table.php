5a36197cb213b9b62afec1ba794e696696a828f5
Janitha Ranasinghe <134924639+janitha479@users.noreply.github.com>
2025-07-28 13:58:46 +0530

    DB change
    add new column for loan instalment count

--- database/migrations/2025_06_30_065254_create_loans_table.php
+++ database/migrations/2025_06_30_065254_create_loans_table.php
@@ -19,6 +19,8 @@ return new class extends Migration {
             $table->decimal('installment_amount', 10, 2);
             $table->date('start_from');
             $table->boolean('with_interest')->default(false);
+            $table->integer('installment_count')->nullable();
+            $table->enum('status', ['active', 'completed'])->default('active');
 
             $table->softDeletes();
             $table->timestamps();
